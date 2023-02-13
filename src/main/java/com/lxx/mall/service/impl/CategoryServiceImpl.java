2f59b879d97a627f97231236ea7f00130a086dc4
lin-xiuxian <2625453652@qq.com>
2023-02-13 22:54:08 +0800

    使用 redis 做缓存

--- src/main/java/com/lxx/mall/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/lxx/mall/service/impl/CategoryServiceImpl.java
@@ -11,6 +11,7 @@ import com.lxx.mall.model.vo.CategoryVO;
 import com.lxx.mall.service.CategoryService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
@@ -77,6 +78,7 @@ public class CategoryServiceImpl implements CategoryService {
     }
 
     @Override
+    @Cacheable(value = "listCategoryForCustomer")
     public List<CategoryVO> listCategoryForCustomer(){
         ArrayList<CategoryVO> categoryVOList = new ArrayList<>();
         recursivelyFindCategories(categoryVOList, 0);
